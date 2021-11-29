61a835b577ecfc6874ac84f05dc8c4d664049165
Samilli Santos <samilli.santos28@gmail.com>
2021-11-29 09:07:50 -0300

    Inclusão de tabelas Admin, Livros, Capitulos e Midia Capitulos + Insert de livros

---
+++ db/Inserts-livros.sql
@@ -0,0 +1,143 @@
+CREATE DATABASE rota66;
+USE rota66;
+DROP DATABASE rota66;
+
+/*Inserts Livros Antigo Testamento*/
+
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Gêneses", "1º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Êxodo", "2º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Levítico", "3º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Números", "4º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Deuteronômio", "5º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Josué", "6º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Juízes", "7º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Rute", "8º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("1 Samuel", "9º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("2 Samuel", "10º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("1 Reis", "11º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("2 Reis", "12º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("1 Crônicas", "13º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("2 Crônicas", "14º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Esdras", "15º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Neemias", "16º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Ester", "17º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Jó", "18º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Salmos", "19º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Provérbios", "20º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Eclesiastes", "21º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Cantares", "22º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Isaías", "23º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Jeremias", "24º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Lamentações", "25º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Ezequiel", "26º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Daniel", "27º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Oseias", "28º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Joel", "29º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Amós", "30º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Obadias", "31º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Jonas", "32º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Miqueias", "33º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Naum", "34º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Abacuque", "35º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Sofonias", "36º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Ageu", "37º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Zacarias", "38º Livro", "Antigo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Malaquias", "39º Livro", "Antigo Testamento");
+
+/*Inserts Livros Novo Testamento*/
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Mateus", "40º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Marcos", "41º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Lucas", "42º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("João", "43º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Atos", "44º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Romanos", "45º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("1 Coríntios", "46º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("2 Coríntios", "47º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Gálatas", "48º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Efésios", "49º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Filipenses", "50º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Colossenses", "51º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("1 Tessalonicenses", "52º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("2 Tessalonicenses", "53º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("1 Timóteo", "54º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("2 Timóteo", "55º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Tito", "56º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Filemon", "57º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Hebreus", "58º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Tiago", "59º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("1 Pedro", "60º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("2 Pedro", "61º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("1 João", "62º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("2 João", "63º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("3 João", "64º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Judas", "65º Livro", "Novo Testamento");
+INSERT INTO livros (nome_livro, desc_livro, testamento)
+VALUES ("Apocalipse", "66º Livro", "Novo Testamento");
+
+select * from livros;
+
